6b6d18fe927c6c72e421801686048653c2176dc2
aswarbs <amberswarbrick1@gmail.com>
2024-06-15 17:56:58 +0100

    handle map

--- src_server/handle_map.py
+++ src_server/handle_map.py
@@ -1,6 +1,12 @@
 from enum import Enum
 import tkinter as tk
 from threading import Thread
+import math
+import numpy as np
+
+
+# SPACING BETWEEN PUMPS ON WALL
+SPACING_OF_PUMPS = 5
 
 class Cell(Enum):
     WALL = 1
@@ -19,7 +25,7 @@ class MapHandler():
         self.map = self.create_test_map()
         self.heatmap = self.create_test_heatmap()
 
-        self.display = False
+        self.display = True
         if(self.display): 
             self.thread = Thread(target=self.display_map)
             self.thread.start()
@@ -39,10 +45,49 @@ class MapHandler():
 
         # return the best, and the heatmap
 
-        pos = self.find_walls_start_end(self.map)
-        print(pos)
+        lengths = self.find_walls_start_end(self.map)
+
+        
+        possible_pump_positions = self.find_pump_positions(lengths)
+        print(possible_pump_positions)
+
+        
 
         return self.map, self.heatmap
+    
+
+
+    
+
+    def find_pump_positions(self, lengths):
+
+        # lengths = vertical and horizontal lengths
+        for x in lengths:
+                for l in x:
+                
+                    length = l[2][0] + l[2][1]
+
+                    # Determine the number of positions
+                    num_positions = (length // SPACING_OF_PUMPS) + 1
+                    
+                    if num_positions == 0:
+                        return []
+                    
+                    # Calculate the direction vector
+                    direction = (l[1] - l[0]) / length
+                    
+                    # Generate the positions
+                    positions = [tuple(l[0] + direction * SPACING_OF_PUMPS * i) for i in range(1, num_positions)]
+                    
+                    return positions
+
+                print(f"{l} pos: {positions}")
+                    
+
+        
+    
+
+
 
     def find_walls_start_end(self,matrix):
         def horizontal_stretches(self,matrix):
@@ -55,10 +100,13 @@ class MapHandler():
                         while end < len(matrix[i]) and matrix[i][end] == 1:
                             end += 1
                         if end - start > 1:  # At least 2 consecutive 1s
-                            horizontal_result.append(((i, start), (i, end - 1)))
+                            horizontal_result.append(((i, start), (i, end - 1), (0, end-start-1)))
                         start = end
                     else:
                         start += 1
+
+                # find the midpoints of each of these pairs
+
             return horizontal_result
 
         def vertical_stretches(self,matrix):
@@ -71,7 +119,7 @@ class MapHandler():
                         while end < len(matrix) and matrix[end][j] == 1:
                             end += 1
                         if end - start > 1:  # At least 2 consecutive 1s
-                            vertical_result.append(((start, j), (end - 1, j)))
+                            vertical_result.append(((start, j), (end - 1, j), (end-start-1, 0)))
                         start = end
                     else:
                         start += 1
@@ -81,8 +129,7 @@ class MapHandler():
         vertical_result = vertical_stretches(self,matrix)
         
         return horizontal_result, vertical_result
-        
-        
+                
     def create_test_heatmap(self):
 
         return [[20] * 10] * 10
@@ -101,8 +148,6 @@ class MapHandler():
             [Cell.WALL.value] * 10
         ]
 
-    
-
     def display_map(self):
 
         self.window = tk.Tk()
@@ -120,7 +165,6 @@ class MapHandler():
 
         self.window.mainloop()
         
-
     def get_color(self, cell):
         color_map = {
             Cell.WALL.value: 'purple',
